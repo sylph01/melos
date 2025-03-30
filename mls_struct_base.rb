58e9e82f4f037371f662696ddb1b1bd4fdcc755b
Ryo Kajiwara <sylph01@s01.ninja>
2025-03-30 23:18:34 +0900

    organize code

---
+++ mls_struct_base.rb
@@ -0,0 +1,159 @@
+module MLSStruct; end
+
+class MLSStruct::Base
+  def initialize(buf)
+    context, _ = deserialize(buf)
+    set_instance_vars(context)
+    self
+  end
+
+  def self.new_and_rest(buf)
+    instance = self.allocate
+    context, buf = instance.send(:deserialize, buf)
+    instance.send(:set_instance_vars, context)
+    [instance, buf]
+  end
+
+  def raw
+    buf = ''
+    self.class::STRUCT.each do |elem|
+      case elem[1]
+      when :select
+        value = self.instance_variable_get("@#{elem[0]}")
+        buf += serialize_select_elem(value, elem[3])
+      else
+        value = self.instance_variable_get("@#{elem[0]}")
+        buf += serialize_elem(value, elem[1])
+      end
+    end
+    buf
+  end
+
+  def self.vecs(buf)
+    value, buf = String.parse_vec(buf)
+    array = []
+    while (value.bytesize > 0)
+      current_instance, value = String.parse_vec(value)
+      array << current_instance
+    end
+    [array, buf]
+  end
+
+  # context here takes a hash
+  # returns [value, rest_of_buffer]
+  # value could return nil, which means predicate was not applicable
+  # predicate takes the context and returns true or false
+  def deserialize_select_elem_with_context(buf, context, predicate, type, type_param)
+    if predicate.(context)
+      deserialize_elem(buf, type, type_param)
+    else
+      [nil, buf]
+    end
+  end
+
+  private
+  def deserialize(buf)
+    context = []
+    self.class::STRUCT.each do |elem|
+      puts elem[0]
+      case elem[1]
+      when :select
+        value, buf = deserialize_select_elem_with_context(buf, context.to_h, elem[2], elem[3], elem[4])
+        context << [elem[0], value]
+      else
+        value, buf = deserialize_elem(buf, elem[1], elem[2])
+        context << [elem[0], value]
+      end
+    end
+    [context, buf]
+  end
+
+  def set_instance_vars(context)
+    context.each do |elem|
+      self.instance_variable_set("@#{elem[0]}", elem[1])
+    end
+  end
+
+  def deserialize_elem(buf, type, type_param)
+    case type
+    when :uint8
+      value = buf.byteslice(0, 1).unpack1('C')
+      buf = buf.byteslice(1..)
+    when :uint16
+      value = buf.byteslice(0, 2).unpack1('S>')
+      buf = buf.byteslice(2..)
+    when :uint32
+      value = buf.byteslice(0, 4).unpack1('L>')
+      buf = buf.byteslice(4..)
+    when :uint64
+      value = buf.byteslice(0, 8).unpack1('Q>')
+      buf = buf.byteslice(8..)
+    when :vec
+      value, buf = String.parse_vec(buf)
+    when :vecs
+      value, buf = MLSStruct::Base.vecs(buf)
+    when :class
+      value, buf = type_param.send(:new_and_rest, buf)
+    when :classes
+      vec, buf = String.parse_vec(buf)
+      value = []
+      while (vec.bytesize > 0)
+        current_instance, vec = type_param.send(:new_and_rest, vec)
+        value << current_instance
+      end
+    when :optional
+      presence = buf.byteslice(0, 1).unpack1('C')
+      case presence
+      when 0
+        value = nil
+        buf = buf.byteslice(1..)
+      when 1
+        # as of RFC 9420, optional always takes a class
+        value, buf = type_param.send(:new_and_rest, buf)
+      end
+    when :opaque
+      value = buf.byteslice(0, type_param.to_i)
+      buf = buf.byteslice((type_param.to_i)..)
+    end
+    [value, buf]
+  end
+
+  # take a name and type
+  def serialize_elem(value, type)
+    case type
+    when :uint8
+      [value].pack('C')
+    when :uint16
+      [value].pack('S>')
+    when :uint32
+      [value].pack('L>')
+    when :uint64
+      [value].pack('Q>')
+    when :vec
+      value.to_vec
+    when :vecs
+      value.map(&:to_vec).join.to_vec
+    when :class
+      value.raw
+    when :classes
+      value.map(&:raw).join.to_vec
+    when :optional
+      if value.nil?
+        [0].pack('C')
+      else
+        # as of RFC 9420, optional always takes a class
+        [1].pack('C') + value.raw
+      end
+    when :opaque
+      value
+    end
+  end
+
+  def serialize_select_elem(value, type)
+    if value.nil?
+      ''
+    else
+      serialize_elem(value, type)
+    end
+  end
+end
---
+++ vec_base.rb
@@ -0,0 +1,77 @@
+def from_hex(hex)
+  [hex].pack('H*')
+end
+
+def to_hex(bin)
+  bin.unpack1('H*')
+end
+
+class String
+  def to_vec
+    header = []
+    len = self.bytesize
+    case len
+    when 0..63
+      header[0] = len
+    when 64..16383
+      header[0] = (1 << 6) || ((len & 0x3f00) >> 8)
+      header[1] = len & 0x00ff
+    when 16384..1073741823
+      header[0] = (2 << 6) || ((len & 0x3f000000) >> 24)
+      header[1] = ((len & 0x00ff0000) >> 16)
+      header[2] = ((len & 0x0000ff00) >> 8)
+      header[3] = len & 0x000000ff
+    else
+      raise ArgumentError.new('too long to be encoded in variable length vector')
+    end
+
+    header.pack('C*') + self
+  end
+
+  def self.parse_vec(vec)
+    prefix = vec[0].ord >> 6
+    case prefix
+    when 0
+      length = vec[0].ord & 0x3f
+      str = vec[1, length]
+      rest = vec[(1 + length)..]
+    when 1
+      length = (vec[0].ord & 0x3f << 8) + vec[1].ord
+      str = vec[2, length]
+      rest = vec[(2 + length)..]
+    when 2
+      length = (vec[0].ord & 0x3f << 24) + (vec[1].ord << 16) + (vec[2].ord << 8) + vec[3].ord
+      str = vec[4, length]
+      rest = vec[(4 + length)..]
+    else
+      raise ArgumentError.new('invalid header')
+    end
+
+    puts length
+    puts to_hex(str)
+    puts to_hex(rest ? rest : '')
+    [str, rest]
+  end
+
+  def self.get_first_vec(vec)
+    prefix = vec[0].ord >> 6
+    case prefix
+    when 0
+      length = vec[0].ord & 0x3f
+      first_vec = vec[0, 1 + length]
+      rest = vec[(1 + length)..]
+    when 1
+      length = (vec[0].ord & 0x3f << 8) + vec[1].ord
+      first_vec = vec[0, 2 + length]
+      rest = vec[(2 + length)..]
+    when 2
+      length = (vec[0].ord & 0x3f << 24) + (vec[1].ord << 16) + (vec[2].ord << 8) + vec[3].ord
+      first_vec = vec[0, 4 + length]
+      rest = vec[(4 + length)..]
+    else
+      raise ArgumentError.new('invalid header')
+    end
+
+    [first_vec, rest]
+  end
+end
