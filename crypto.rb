641475303527dafbf615b74b9bd354c0572137e1
Ryo Kajiwara <sylph01@s01.ninja>
2025-04-01 19:41:57 +0900

    Crypto (cipher suite 1)

---
+++ crypto.rb
@@ -0,0 +1,75 @@
+require 'openssl'
+require 'hpke'
+require_relative 'vec_base'
+
+module MLS; end
+
+class MLS::Crypto
+  DIGEST = OpenSSL::Digest::SHA256
+  KDF = HPKE::HKDF.new(:sha256)
+  HPKE = HPKE.new(:x25519, :sha256, :sha256, :aes_128_gcm)
+
+  def self.ref_hash(label, value)
+    ref_hash_input = label.to_vec + value.to_vec
+    DIGEST.digest(ref_hash_input)
+  end
+
+  def self.make_keypackage_ref(value)
+    self.ref_hash("MLS 1.0 KeyPackage Reference", value)
+  end
+
+  def self.make_proposal_ref(value)
+    self.ref_hash("MLS 1.0 Proposal Reference", value)
+  end
+
+  def self.expand_with_label(secret, label, context, length)
+    kdf_label = [length].pack('S>') + ("MLS 1.0 " + label).to_vec + context.to_vec
+    KDF.expand(secret, kdf_label, length)
+  end
+
+  def self.derive_secret(secret, label)
+    expand_with_label(secret, label, "", KDF.n_h)
+  end
+
+  def self.derive_tree_secret(secret, label, generation, length)
+    generation_in_uint32 = [generation].pack('L>')
+    expand_with_label(secret, label, generation_in_uint32, length)
+  end
+
+  def self.seal_base(pkr, info, aad, pt)
+    context = HPKE.setup_base_s(pkr, info)
+    enc = context[:enc]
+    ctx = context[:context_s]
+    ct = ctx.seal(aad, pt)
+    [enc, ct]
+  end
+
+  def self.open_base(enc, skr, info, aad, ct)
+    ctx = HPKE.setup_base_r(enc, skr, info)
+    ctx.open(aad, ct)
+  end
+
+  def self.encrypt_with_label(public_key, label, context, plaintext)
+    encrypt_context = ("MLS 1.0 " + label).to_vec + context.to_vec
+    pkey = OpenSSL::PKey.new_raw_public_key("X25519", public_key)
+    seal_base(pkey, encrypt_context, "", plaintext)
+  end
+
+  def self.decrypt_with_label(private_key, label, context, kem_output, ciphertext)
+    encrypt_context = ("MLS 1.0 " + label).to_vec + context.to_vec
+    pkey = OpenSSL::PKey.new_raw_private_key("X25519", private_key)
+    open_base(kem_output, pkey, encrypt_context, "", ciphertext)
+  end
+
+  def self.sign_with_label(signature_key, label, content)
+    skey = OpenSSL::PKey.new_raw_private_key("ED25519", signature_key)
+    sign_content = ("MLS 1.0 " + label).to_vec + content.to_vec
+    skey.sign(nil, sign_content)
+  end
+
+  def self.verify_with_label(verification_key, label, content, signature_value)
+    vkey = OpenSSL::PKey.new_raw_public_key("ED25519", verification_key)
+    sign_content = ("MLS 1.0 " + label).to_vec + content.to_vec
+    vkey.verify(nil, signature_value, sign_content)
+  end
+end
---
+++ test_crypto.rb
@@ -0,0 +1,96 @@
+require 'json'
+require_relative 'crypto'
+require_relative 'vec_base'
+require 'minitest'
+include Minitest::Assertions
+
+class << self
+attr_accessor :assertions
+end
+self.assertions = 0
+
+crypto_vectors = JSON.parse(File.read('test_vectors/crypto-basics.json'))
+
+crypto_vectors[0..0].each_with_index do |vector, index|
+  # RefHash
+  ref_hash = MLS::Crypto.ref_hash(vector['ref_hash']['label'], from_hex(vector['ref_hash']['value']))
+  assert_equal to_hex(ref_hash), vector['ref_hash']['out']
+  puts "[s] ref_hash"
+
+  # expand_with_label
+  out = MLS::Crypto.expand_with_label(
+    from_hex(vector['expand_with_label']['secret']),
+    vector['expand_with_label']['label'],
+    from_hex(vector['expand_with_label']['context']),
+    vector['expand_with_label']['length']
+  )
+  assert_equal to_hex(out), vector['expand_with_label']['out']
+  puts "[s] expand_with_label"
+
+  # derive_secret
+  out = MLS::Crypto.derive_secret(
+    from_hex(vector['derive_secret']['secret']),
+    vector['derive_secret']['label']
+  )
+  assert_equal to_hex(out), vector['derive_secret']['out']
+  puts "[s] derive_secret"
+
+  # derive_tree_secret
+  out = MLS::Crypto.derive_tree_secret(
+    from_hex(vector['derive_tree_secret']['secret']),
+    vector['derive_tree_secret']['label'],
+    vector['derive_tree_secret']['generation'],
+    vector['derive_tree_secret']['length'],
+  )
+  assert_equal to_hex(out), vector['derive_tree_secret']['out']
+  puts "[s] derive_tree_secret"
+
+  # encrypt_with_label
+  priv = from_hex(vector['encrypt_with_label']['priv'])
+  pub = from_hex(vector['encrypt_with_label']['pub'])
+  label = vector['encrypt_with_label']['label']
+  context = from_hex(vector['encrypt_with_label']['context'])
+  plaintext = from_hex(vector['encrypt_with_label']['plaintext'])
+  kem_output = from_hex(vector['encrypt_with_label']['kem_output'])
+  ciphertext = from_hex(vector['encrypt_with_label']['ciphertext'])
+
+  pt = MLS::Crypto.decrypt_with_label(
+    priv,
+    label,
+    context,
+    kem_output,
+    ciphertext
+  )
+  assert_equal pt, plaintext
+
+  kem_output_candidate, ciphertext_candidate = MLS::Crypto.encrypt_with_label(
+    pub,
+    label,
+    context,
+    plaintext
+  )
+  pt2 = MLS::Crypto.decrypt_with_label(
+    priv,
+    label,
+    context,
+    kem_output_candidate,
+    ciphertext_candidate
+  )
+  assert_equal pt2, plaintext
+  puts "[s] encrypt_with_label"
+
+  # sign_with_label
+  priv = from_hex(vector['sign_with_label']['priv'])
+  pub = from_hex(vector['sign_with_label']['pub'])
+  content = from_hex(vector['sign_with_label']['content'])
+  label = vector['sign_with_label']['label']
+  signature = from_hex(vector['sign_with_label']['signature'])
+
+  assert_equal true, MLS::Crypto.verify_with_label(
+    pub, label, content, signature)
+  assert_equal true, MLS::Crypto.verify_with_label(
+    pub, label, content,
+    MLS::Crypto.sign_with_label(priv, label, content)
+  )
+  puts "[s] sign_with_label"
+end
