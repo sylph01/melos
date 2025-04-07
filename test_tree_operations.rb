d62c074852a64cc523eaca86e0a8818f10f13c61
Ryo Kajiwara <sylph01@s01.ninja>
2025-04-07 13:37:13 +0900

    refactor: check_tree_equality

--- test_tree_operations.rb
+++ test_tree_operations.rb
@@ -11,6 +11,17 @@ attr_accessor :assertions
 end
 self.assertions = 0
 
+def check_tree_equality(left, right)
+  assert_equal left.count, right.count
+  left.each_with_index do |node, index|
+    if node.nil?
+      assert_nil right[index]
+    else
+      assert_equal node.raw, right[index].raw
+    end
+  end
+end
+
 vectors = JSON.load_file('test_vectors/tree-operations.json')
 vectors.each do |vec|
   tree_before = MLS::Struct::RatchetTree.parse(from_hex(vec['tree_before']))
@@ -23,14 +34,11 @@ vectors.each do |vec|
     node = MLSStruct::Node.new_leaf_node(prop.add.key_package.leaf_node)
     MLS::Struct::RatchetTree.add_leaf_node(tree_before, node)
     # check tree equality
-    assert_equal tree_before.count, tree_after.count
-    tree_before.each_with_index do |node, index|
-      if node.nil?
-        assert_nil tree_after[index]
-      else
-        assert_equal node&.raw, tree_after[index]&.raw
-      end
-    end
+    check_tree_equality(tree_before, tree_after)
     puts "[pass] Application of Add"
+  elsif prop.update
+
+  elsif prop.remove
+
   end
 end
