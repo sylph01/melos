7c5e65073ba3f50217a5e6fb572ca75ff9bbcd90
Ryo Kajiwara <sylph01@s01.ninja>
2025-04-10 09:19:44 +0900

    [WIP] rewrite test_deserialization with assertions

--- test_deserialization.rb
+++ test_deserialization.rb
@@ -1,14 +1,22 @@
 require 'json'
+require_relative 'lib/mls'
+require 'minitest'
+include Minitest::Assertions
+
+class << self
+  attr_accessor :assertions
+end
+self.assertions = 0
+
 deserialization_vectors = JSON.parse(File.read('test_vectors/deserialization.json'))
 
-require_relative 'vec_base'
-
-deserialization_vectors.each do |v|
+deserialization_vectors.each_with_index do |v, idx|
+  puts "vector #{idx}:"
   header = [v['vlbytes_header']].pack('H*')
-  length = read_varint(header)
-  puts length
-  puts v['length']
-  puts write_varint(length).unpack1('H*')
-  puts v['vlbytes_header']
-  puts
+  length = MLS::Vec.read_varint(header)
+  assert_equal length, v['length']
+  puts "[pass] length matches"
+
+  assert_equal MLS::Vec.write_varint(length).unpack1('H*'), v['vlbytes_header']
+  puts "[pass] varint header matches"
 end
