e92468211a33aa2335e2bf1dab6a76671e0c7711
Ryo Kajiwara <sylph01@s01.ninja>
2025-08-04 14:21:28 +0900

    [WIP] join_with_welcome

--- lib/melos/group.rb
+++ lib/melos/group.rb
@@ -3,4 +3,26 @@ class Melos::Group
     # TODO: check if cipher_suite is defined
     @cipher_suite = cipher_suite
   end
+  
+  # message is the raw message,
+  # key_package is a Melos::Struct::MLSMessage that has a KeyPackage type
+  def join_with_welcome(message, key_package)
+    message_obj = Melos::Struct::MLSMessage.new(message)
+    if message_obj.welcome.nil?
+      raise ArgumentError.new('Wrong message type (need a Welcome message)')
+    end
+    welcome = message_obj.welcome
+    kp_ref = key_package.key_package.ref(suite)
+    encrypted_group_secrets = welcome.secrets.find { _1.new_member == kp_ref }&.encrypted_group_secrets
+    group_secrets = Melos::Struct::GroupSecrets.new(
+      Melos::Crypto.decrypt_with_label(
+      suite,
+      init_priv,
+      "Welcome",
+      welcome.welcome.encrypted_group_info,
+      egs.kem_output,
+      egs.ciphertext
+      )
+    )
+  end
 end
